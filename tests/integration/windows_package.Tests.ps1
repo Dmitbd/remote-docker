fa06fde4c5788ad094080beb4e819bccdc5e0ddc
Dmitbd <105349881+Dmitbd@users.noreply.github.com>
2026-08-08 01:32:35 +0300

    test: match managed runtime status path

--- tests/integration/windows_package.Tests.ps1
+++ tests/integration/windows_package.Tests.ps1
@@ -70,7 +70,7 @@ Describe 'Windows package contract' {
         $script | Should -Match 'Join-Path \$programData ''RemoteDocker'''
         $script | Should -Match 'Join-Path \$programFiles ''Remote Docker\\RemoteDockerAgent\.exe'''
         $script | Should -Match 'Invoke-External\s+-FilePath\s+\$agentExecutable\s+-ArgumentList\s+@\(''--prepare-wsl''\)'
-        $script | Should -Match '''remote-docker-remote'',\s*''runtime-status'''
+        $script | Should -Match '''/usr/local/bin/remote-docker-remote'',\s*''runtime-status'''
         $script | Should -Not -Match 'syncthing generate --home=/var/lib/remote-docker|ssh-keygen'
         $script | Should -Match 'Join-Path \$PSScriptRoot ''\.\.\\assets\\remote-docker-rootfs\.tar\.zst'''
         $script | Should -Match '-Profile\s+Private'
